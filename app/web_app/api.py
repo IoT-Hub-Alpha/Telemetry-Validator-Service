a908b0274ed9e1bb7a6e5ea06cae9de97e4d945c
AntonKoluh <antonkoluh1@gmail.com>
2026-04-07 13:00:54 +0300

    Refactor api.py by removing CORS middleware

--- app/web_app/api.py
+++ app/web_app/api.py
@@ -1,5 +1,4 @@
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
 from iot_logging import FastAPIRequestContextMiddleware, StructuredJsonFormatter
 import logging
 
@@ -9,17 +8,6 @@ app = FastAPI()
 logging.basicConfig(level=logging.INFO)
 for handler in logging.root.handlers:
     handler.setFormatter(StructuredJsonFormatter())
-
-app.add_middleware(FastAPIRequestContextMiddleware)
-
-
-# allow everyone, since its dev, why the hell not, right???
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 
 
 @app.get("/health")

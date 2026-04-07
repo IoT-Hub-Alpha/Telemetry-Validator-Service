ea3852602609487f2982cac6686494c735bf49bd
Anton Koluh <antonkoluh1@gmail.com>
2026-04-07 12:58:02 +0300

    removed cors

--- app/web_app/api.py
+++ app/web_app/api.py
@@ -1,5 +1,4 @@
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
 from iot_logging import FastAPIRequestContextMiddleware, StructuredJsonFormatter
 import logging
 
@@ -11,15 +10,6 @@ for handler in logging.root.handlers:
     handler.setFormatter(StructuredJsonFormatter())
 
 app.add_middleware(FastAPIRequestContextMiddleware)
-
-
-# allow everyone, since its dev, why the hell not, right???
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 
 
 @app.get("/health")
